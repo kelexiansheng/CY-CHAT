6590da91b4d6e5b7306c9ab2abc14875d551c971
kelexiansheng <130484852+kelexiansheng@users.noreply.github.com>
2023-04-13 16:50:34 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -167,9 +167,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>CY CHAT</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            QQ:442543224
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
